06fb7ef68939c277a633b9f99cf2481514ca80f7
abdullah-25 <70604820+abdullah-25@users.noreply.github.com>
2021-09-19 00:08:59 -0400

    Update App.js

--- quick-receipts-app/App.js
+++ quick-receipts-app/App.js
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+
+
+export default class App extends React.Component {
+  render() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.logoWrapper}>
+      
+      <Image 
+        style= {{ width: 50, height: 45}}
+        source= {require('./assets/logo_final.png')} />
+        
+        <Text style={styles.title} >QuickReceipts</Text>
+      
+      </View>
+
+      <TouchableOpacity>
+        <View style={styles.appButtonContainer}>
+        <Text style={styles.appButtonText}>Start</Text>
+        </View>
+      </TouchableOpacity>
+     
+      
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1A1A1A',
+    
+  },
+  title: {
+    fontFamily: 'normal',
+  
+    fontSize: 34,
+    color: "#FFFFFF",
+    
+  },
+  logoWrapper:{
+    paddingTop: 140,
+    paddingHorizontal: 70,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  },
+  appButtonContainer:{
+    position: 'absolute',
+    bottom:-550,
+    left:150,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 20,
+    elevation: 3,
+    backgroundColor: 'white',
+
+
   },
+  appButtonText:{
+    fontSize: 20,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+
+    
+    
+
+},
+  
 });
